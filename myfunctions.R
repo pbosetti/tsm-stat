a738237fb768e02ae37fad3dff1d0304c74ec7c0
Paolo Bosetti <paolo.bosetti@unitn.it>
2022-01-07 18:39:13 +0100

    Minor edits

--- myfunctions.R
+++ myfunctions.R
@@ -9,10 +9,13 @@ tukey.plot <- function(t) {
   name <- paste("Differences in mean levels of", names(t)[1])
   t[[1]] %>% 
     data.frame %>%
-    rownames_to_column() %>%
-      ggplot(aes(x=.data[1])) + 
+    rownames_to_column("pair") %>%
+      ggplot(aes(x=pair))+ 
       geom_point(aes(y=diff)) + 
       geom_linerange(aes(ymin=lwr, ymax=upr)) + 
       geom_hline(yintercept=0) + 
+      labs(title=paste0((attr(t, "conf.level")*100), "% family-wise confidence level")) +
+      ylab(name) + 
+      xlab("") +
       coord_flip()
 }
